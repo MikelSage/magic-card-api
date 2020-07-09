fdfb9f313af7447f74f80c451b3e521e83581854
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 17:43:11 -0600

    add rescue block to cards controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::CardsController < ApplicationController
+  rescue_from MagicCardApiService::InvalidParametersError do |e|
+    render json: {
+      errors: [
+        detail: e.message
+      ]
+    }, status: 400
+  end
+
   def index
     cards = Card.find_by_query(params)
     render json: CardSerializer.new(cards, is_collection: true)
