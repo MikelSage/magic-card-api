cbaa989f13fc1cb7f6ab1db722cd1ab19b3ef052
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 13:49:11 -0600

    add creation of search object in service

--- app/services/magic_card_api_service.rb
+++ app/services/magic_card_api_service.rb
@@ -3,14 +3,17 @@ class MagicCardApiService
   def initialize(search_params)
     @conn = Faraday.new(url: 'https://api.magicthegathering.io/v1/')
     @params = search_params
+    @search = Search.find_or_create_by(@params) #convert to method to create search and check for cache
   end
 
   def self.find_cards(search_params)
     new(search_params).find_cards
+     # add checking for cached response here
   end
 
   def find_cards
     response = @conn.get('cards', @search_params)
-    results = JSON.parse(response.body, symbolize_names: true)
+    results = JSON.parse(response.body, symbolize_names: true)[:cards]
+    # cache results
   end
 end
