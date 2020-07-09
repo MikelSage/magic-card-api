166f34cdce85b8c81cb1eb21bea7cb6310c19f3b
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 14:25:42 -0600

    add basic skeleton of request specs

--- spec/requests/api/v1/cards_request_spec.rb
+++ spec/requests/api/v1/cards_request_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Cards", type: :request do
+  describe 'index' do
+    it 'returns a list of cards with no params' do
 
+    end
+
+    it 'returns a list of cards with filtered by name' do
+
+    end
+
+    it 'returns a list of cards filtered by color' do
+
+    end
+
+    it 'returns a list of cards filtered by color and sorted by name' do
+
+    end
+
+    context 'errors' do
+      it 'returns valid JSON when passed invalid parameters' do
+
+      end
+    end
+  end
 end
