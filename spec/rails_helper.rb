70bc7061787790e420fe225e090cd595ddbb2193
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 14:52:12 -0600

    fix spelling mistake in VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
-  config.allow_http_connection_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = true
 end
 
 RSpec.configure do |config|
