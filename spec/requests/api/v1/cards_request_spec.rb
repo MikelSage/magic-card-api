6f9a6048924615d7b0da7ae84fa2a5fdea0a5887
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 14:53:23 -0600

    add basic test for index with no params

--- spec/requests/api/v1/cards_request_spec.rb
+++ spec/requests/api/v1/cards_request_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Cards", type: :request do
   describe 'index' do
     it 'returns a list of cards with no params' do
+      VCR.use_cassette('/requests/index_no_params') do
+        get api_v1_cards_path
 
+        result = JSON.parse(response.body).symbolize_keys
+
+        expect(response).to have_http_status(:success)
+      end
     end
 
     it 'returns a list of cards with filtered by name' do
