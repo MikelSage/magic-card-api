90adf2630a544bc4d14dcb7c2b448cd77e5d11db
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 13:49:31 -0600

    add basic card poro model

---
+++ app/models/card.rb
@@ -0,0 +1,29 @@
+# PORO for dealing with Cards from MTG API
+class Card
+  attr_reader :name,
+              :mana_cost,
+              :converter_mana_cost,
+              :power,
+              :toughness,
+              :types,
+              :rarity,
+              :colors
+
+  def initialize(attrs)
+    @name = attrs[:name]
+    @mana_cost = attrs[:manaCost]
+    @converted_mana_cost = attrs[:cmc]
+    @power = attrs[:power]
+    @toughness = attrs[:toughness]
+    @types = attrs[:types]
+    @rarity = attrs[:rarity]
+    @colors = attrs[:colors]
+  end
+
+  def self.find_by_query(query)
+    raw_data = MagicCardApiService.find_cards(query)
+    raw_data.map do |raw_card|
+      new(raw_card)
+    end
+  end
+end
