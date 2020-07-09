9321bd9a32d04ec441c0f26af6b274fc3bace565
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 14:25:17 -0600

    add basic code for pulling cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CardsController < ApplicationController
   def index
-    render json: Card.find_by_query(params)
+    cards = Card.find_by_query(params)
+    render json: CardSerializer.new(cards, is_collection: true)
   end
 end
