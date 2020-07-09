2cdf0956da0fdacc2cdc483a2bea70547e576139
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 17:42:55 -0600

    add custom error for if invalid parameters entered

--- app/services/magic_card_api_service.rb
+++ app/services/magic_card_api_service.rb
@@ -1,8 +1,21 @@
 # Service object to consume Magic The Gathering API
 class MagicCardApiService
+  class InvalidParametersError < StandardError # defined here for sake of time
+    def message
+      'Invalid parameters entered. Allowed parameters can be found in
+       the README at https://github.com/MikelSage/magic-card-api'
+    end
+  end
+
   def initialize(search_params)
-    @conn = Faraday.new(url: 'https://api.magicthegathering.io/v1/')
+    @conn = Faraday.new('https://api.magicthegathering.io/v1/') do |c|
+      c.use Faraday::Response::RaiseError
+    end
+
     @query = search_params
+
+    raise InvalidParametersError unless all_parameters_valid?
+
     @search = Search.find_or_create_by(query: @query) #convert to method to create search and check for cache
   end
 
@@ -16,4 +29,14 @@ class MagicCardApiService
     results = JSON.parse(response.body, symbolize_names: true)[:cards]
     # cache results
   end
+
+  private
+
+    def all_parameters_valid?
+      allowed_params = %w[name colors mana_cost cmc power toughness orderBy page pageSize]
+      # extremely contrived way to raise error, couldn't think of another way in time constraints
+      @query.to_unsafe_hash.except('action', 'controller').all? do |param_name, _param_value|
+        allowed_params.include?(param_name)
+      end
+    end
 end
