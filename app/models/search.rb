c964eda23dea3f427cdea58324661fdbd85d33a8
MikelSage <michael.sagapolutele@yahoo.com>
2020-07-09 13:13:40 -0600

    add uniqueness validator and presence validator to search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,3 @@
 class Search < ApplicationRecord
+  validates :query, uniqueness: true, presence: true
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'does not allow non-unique queries to be saved' do
+      query = { colors: '["green", "blue"]', types: 'creature' }
+      query_with_different_order = { types: 'creature', colors: '["green", "blue"]'}
+
+      valid_search = Search.create(query: query)
+      invalid_search = Search.create(query: query_with_different_order)
+
+      expect(valid_search.valid?).to be true
+      expect(invalid_search.valid?).to be false
+      expect(invalid_search.errors.messages[:query]).to include('has already been taken')
+    end
+  end
 end
